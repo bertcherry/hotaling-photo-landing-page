87f84fb4d95bc35b8f421502c5dba51f0d2a4fcb
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-04-26 16:02:46 -0700

    add dropdown module

---
+++ src/dropdown.js
@@ -0,0 +1,22 @@
+const activateDropdowns = () => {
+    const dropdowns = document.querySelectorAll('.dropdown');
+    dropdowns.forEach(dropdown => {
+        const dropdownParent = dropdown.parentElement;
+        dropdownParent.onmouseover = showDropdown;
+        dropdownParent.onfocus = showDropdown;
+        dropdownParent.onmouseout = hideDropdown;
+        dropdownParent.onblur = hideDropdown;
+    });
+}
+
+function showDropdown(e) {
+    const element = e.currentTarget.querySelector(':nth-child(2)');
+    element.style.display = 'flex';
+}
+
+function hideDropdown(e) {
+    const element = e.currentTarget.querySelector(':nth-child(2)');
+    element.style.display = 'none';
+}
+
+export { activateDropdowns };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './style.css';
+import { activateDropdowns } from './dropdown';
+
+activateDropdowns();
